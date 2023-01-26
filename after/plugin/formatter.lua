bca590b06277f235686c7e3c873c94397cf428a4
yoquec <alvaro.viejo@protonmail.com>
2023-01-26 22:10:39 +0100

    Added formatter support 🖊 for kotlin (ktlint)

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,24 +1,39 @@
 require("formatter").setup {
-  -- Enable or disable logging
-  logging = true,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-      javascript = {
-          require("formatter.filetypes.javascript").prettier
-      },
-      html = {
-          require("formatter.filetypes.html").prettier
-      },
-      markdown = {
-          require("formatter.filetypes.markdown").prettier
-      },
-      rmd = {
-          require("formatter.filetypes.markdown").prettier
-      },
-      python = {
-          require("formatter.filetypes.python").yapf
-      },
+    -- Enable or disable logging
+    logging = true,
+    -- Set the log level
+    log_level = vim.log.levels.WARN,
+    -- All formatter configurations are opt-in
+    filetype = {
+        javascript = {
+            require("formatter.filetypes.javascript").prettier
+        },
+        html = {
+            require("formatter.filetypes.html").prettier
+        },
+        markdown = {
+            require("formatter.filetypes.markdown").prettier
+        },
+        rmd = {
+            require("formatter.filetypes.markdown").prettier
+        },
+        python = {
+            require("formatter.filetypes.python").yapf
+        },
+        sh = {
+            require("formatter.filetypes.sh").shfmt
+        },
+        kotlin = {
+            function()
+                return {
+                    exe = "ktlint",
+                    args = {
+                        "-F",
+                        "--stdin"
+                    },
+                    stdin = true
+                }
+            end
+        },
     }
-  }
+}
