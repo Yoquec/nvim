18edaa0e342a21d757d38a57095767cea9dc0b21
yoquec <alvaro.viejo@protonmail.com>
2023-02-10 15:59:47 +0100

    Fixed typo in packer.nvim

--- lua/yoquec/packer.lua
+++ lua/yoquec/packer.lua
@@ -1,7 +1,7 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer./vim]]
+vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- packer can manage itself
