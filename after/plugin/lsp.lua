5bbbb92d0e92a7c36897d2534d0af342fe9abba9
yoquec <alvaro.viejo@protonmail.com>
2023-02-10 14:21:25 +0100

    Disabled eslint block (why was that a thing ⁉)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -41,12 +41,6 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
-
-
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
