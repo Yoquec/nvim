7f1deb8d741b43971c41045735582804e952c50c
Yoquec <alvaro.viejo@pm.me>
2023-04-30 21:37:51 +0200

    Changed vim lsp diagnostics keys

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -55,8 +55,8 @@ lsp.on_attach(function(client, bufnr)
     -- Vim diagnostics
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vs", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
 end)
