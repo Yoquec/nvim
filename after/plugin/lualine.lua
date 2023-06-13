9836b6e673976cb5db6c9383c85550f7639729a0
Yoquec <alvaro.viejo@pm.me>
2023-06-13 15:50:58 +0200

    Changed git add sign

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -206,7 +206,7 @@ ins_right {
 
 ins_right {
     'diff',
-    symbols = { added = ' ', modified = '~ ', removed = '- ' },
+    symbols = { added = '+ ', modified = '~ ', removed = '- ' },
     diff_color = {
         added = { fg = colors.green },
         modified = { fg = colors.orange },
