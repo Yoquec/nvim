e8fc4f785461f9276c470eb56fab5578d0959f8f
Yoquec <alvaro.viejo@pm.me>
2023-06-14 17:25:12 +0200

    Fixed typo in Normal float color hl group

--- after/plugin/gruvbox.lua
+++ after/plugin/gruvbox.lua
@@ -67,4 +67,4 @@ vim.cmd.colorscheme("gruvbox")
 
 -- set transparency
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "Normalkloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
