5c3f94a8bc2df6e91ce4da266a082e9ea8aaaffb
Yoquec <alvaro.viejo@pm.me>
2023-09-15 20:09:24 +0200

    Adds neotree binding to create files consistent with netrw

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -1,1 +1,15 @@
 vim.keymap.set('n', "<leader>tf", vim.cmd.NeoTreeShowToggle)
+
+-- mappings consistent with netrw
+require("neo-tree").setup({
+    window = {
+        mappings = {
+            ["%"] = {
+                "add",
+                config = {
+                    show_path = "none"
+                }
+            }
+        }
+    }
+})
