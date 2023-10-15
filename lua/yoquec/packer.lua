d08a5534b5ce57b63cf8da71f8fb96d1a901085e
Yoquec <alvaro.viejo@pm.me>
2023-10-15 12:10:18 +0200

    Fixes telescope download issues for nvim v0.9.4

--- lua/yoquec/packer.lua
+++ lua/yoquec/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.x',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
