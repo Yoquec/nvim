998189cae0b7c2f5ffd2753080691165edc422ad
Yoquec <alvaro.viejo@pm.me>
2023-10-30 20:50:33 +0100

    Adds missing keybindings for LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -37,6 +37,7 @@ cmp.setup({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
@@ -56,3 +57,25 @@ lsp_zero.set_sign_icons({
 vim.diagnostic.config({
     virtual_text = true
 })
+
+lsp_zero.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    -- Cool LSP stuff
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<leader><leader>p', vim.cmd.LspZeroFormat, opts)
+    vim.keymap.set('n', '<leader>vw', vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set('n', '<leader>vr', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>vs', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>va', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+
+    -- Vim diagnostic
+    vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>K', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', '<leader>J', vim.diagnostic.goto_next, opts)
+end)
