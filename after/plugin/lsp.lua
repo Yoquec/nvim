e9a18c0d14c2f9aad7f2cecb442ef914eff68784
Yoquec <alvaro.viejo@pm.me>
2023-12-27 10:17:07 +0100

    fix: Removes unused mapping <C-y> and frees it for copilot

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -69,7 +69,6 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = false }),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
