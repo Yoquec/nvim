b57b086cf35f69089719fb46d146a858015ae7a6
Yoquec <alvaro.viejo@pm.me>
2023-12-28 01:49:40 +0100

    Brought back telescope file browser

--- lua/yoquec/plugins/telescope.lua
+++ lua/yoquec/plugins/telescope.lua
@@ -1,13 +1,35 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-telescope/telescope-file-browser.nvim', 'nvim-lua/plenary.nvim' },
     config = function()
+        local fb_actions = require("telescope").extensions.file_browser.actions
         local builtin = require('telescope.builtin')
+        local telescope_actions = require("telescope.actions")
+
+        require("telescope").setup {
+            extensions = {
+                file_browser = {
+                    theme = "ivy",
+                    path = "%:p:h",
+                    mappings = {
+                        ["n"] = {
+                            ['%'] = fb_actions.create,
+                            D = fb_actions.remove,
+                            ['<C-n>'] = telescope_actions.move_selection_next,
+                            ['<C-p>'] = telescope_actions.move_selection_previous,
+                        },
+                    },
+                },
+            },
+        }
+
+        require("telescope").load_extension "file_browser"
+
         vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>fv', vim.cmd.Ex, {})
+        vim.keymap.set('n', '<leader>fv', [[<cmd>Telescope file_browser<cr>]], {})
         vim.keymap.set('n', '<leader>bi', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fp', builtin.live_grep, {})
     end
