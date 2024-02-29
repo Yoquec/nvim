9f49f546a9fec70cd810c221d5baf157a14e1cc9
Yoquec <alvaro.viejo@pm.me>
2024-02-29 23:11:37 +0100

    feat(luasnip): Loads latex snippets into markdown files

--- lua/yoquec/plugins/snippets.lua
+++ lua/yoquec/plugins/snippets.lua
@@ -6,7 +6,10 @@ return {
         "rafamadriz/friendly-snippets",
     },
     config = function()
+        local ls = require("luasnip")
         require("luasnip.loaders.from_snipmate").lazy_load()
         require("luasnip.loaders.from_vscode").lazy_load()
+        -- Adds latex snippets to markdown files 
+        ls.filetype_extend("markdown", { "tex" })
     end
 }
