a73f05606ffea001b3d0d6804d674be97b946f6c
Alvaro Viejo <alvaro.viejo@pm.me>
2024-04-06 22:03:20 +0000

    feat(dap): Added nvim dap w/ golang support

---
+++ lua/yoquec/plugins/dap.lua
@@ -0,0 +1,23 @@
+return {
+    'mfussenegger/nvim-dap',
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+        "leoluz/nvim-dap-go"
+    },
+    config = function()
+        local dap = require("dap")
+        local dap_ui = require("dap.ui.widgets")
+
+        vim.keymap.set("n", "<leader>dc", dap.continue)
+        vim.keymap.set("n", "<leader>dq", dap.close)
+        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+        vim.keymap.set("n", "<leader>do", dap.step_over)
+        vim.keymap.set("n", "<leader>di", dap.step_into)
+        vim.keymap.set("n", "<leader>dr", dap.repl.open)
+        vim.keymap.set("n", "<leader>dh", dap_ui.hover)
+        vim.keymap.set("n", "<leader>dp", dap_ui.preview)
+
+        -- Golang debug adapter
+        require("dap-go").setup()
+    end
+}
