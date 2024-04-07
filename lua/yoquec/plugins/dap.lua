e6e1ba684461e66087fd137d407188bd3904dd87
Yoquec <alvaro.viejo@pm.me>
2024-04-08 01:00:56 +0200

    feat(dap): Adds javascript node dap config

--- lua/yoquec/plugins/dap.lua
+++ lua/yoquec/plugins/dap.lua
@@ -1,13 +1,12 @@
 return {
     'mfussenegger/nvim-dap',
-    ft = {"go"},
-    dependencies = {
-        "leoluz/nvim-dap-go"
-    },
+    ft = { "go", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    dependencies = { "leoluz/nvim-dap-go" },
     config = function()
         local dap = require("dap")
         local dap_ui = require("dap.ui.widgets")
 
+        -- Keymaps
         vim.keymap.set("n", "<leader>dc", dap.continue)
         vim.keymap.set("n", "<leader>dq", dap.close)
         vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
@@ -19,5 +18,36 @@ return {
 
         -- Golang debug adapter
         require("dap-go").setup()
+
+        -- Javascript node debug adapter
+        dap.adapters["pwa-node"] = {
+            type = "server",
+            host = "localhost",
+            port = "${port}",
+            executable = {
+                command = "node",
+                -- Download from https://github.com/microsoft/vscode-js-debug/releases
+                args = { os.getenv("HOME") .. "/.local/share/js-debug/src/dapDebugServer.js", "${port}" },
+            }
+        }
+
+        for _, language in ipairs({ "javascript", "javascriptreact", "typescript", "typescriptreact" }) do
+            require("dap").configurations[language] = {
+                {
+                    type = "pwa-node",
+                    request = "launch",
+                    name = "Launch file",
+                    program = "${file}",
+                    cwd = "${workspaceFolder}",
+                },
+                {
+                    type = "pwa-node",
+                    request = "attach",
+                    name = "Attach",
+                    processId = require 'dap.utils'.pick_process,
+                    cwd = "${workspaceFolder}",
+                }
+            }
+        end
     end
 }
