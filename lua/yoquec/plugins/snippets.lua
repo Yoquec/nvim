205764dc7544f3770f663839443c3b220466f3ed
Yoquec <alvaro.viejo@pm.me>
2024-05-06 21:19:52 +0200

    feat(luasnip): adds cycle snippets keymap

--- lua/yoquec/plugins/snippets.lua
+++ lua/yoquec/plugins/snippets.lua
@@ -9,7 +9,20 @@ return {
         local ls = require("luasnip")
         require("luasnip.loaders.from_snipmate").lazy_load()
         require("luasnip.loaders.from_vscode").lazy_load()
+
         -- Adds latex snippets to markdown files 
         ls.filetype_extend("markdown", { "tex" })
+
+        -- Adds keymaps for snippet choices
+        vim.keymap.set({"i", "n"}, "<A-j>", function ()
+            if ls.choice_active() then
+                ls.change_choice(1)
+            end
+        end)
+        vim.keymap.set({"i", "n"}, "<A-k>", function ()
+            if ls.choice_active() then
+                ls.change_choice(-1)
+            end
+        end)
     end
 }
