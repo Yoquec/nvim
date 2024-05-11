2dee93d9141dd20f68ff9638a012be4a8be6dafd
Yoquec <alvaro.viejo@pm.me>
2024-05-11 14:02:28 +0200

    feat: adds keymap for Colorized attach to a buffer

--- lua/yoquec/plugins/init.lua
+++ lua/yoquec/plugins/init.lua
@@ -1,9 +1,15 @@
 return {
     -- Plugins that don't need a configuration
-    { "jalvesaq/Nvim-R",             ft = { "r" } },
-    { 'windwp/nvim-autopairs',       event = 'InsertEnter' },
-    { "tpope/vim-repeat",            event = "InsertEnter" },
-    { 'tpope/vim-surround',          event = { "BufReadPost", "BufNewFile" } },
-    { 'tpope/vim-commentary',        event = { "BufReadPost", "BufNewFile" } },
-    { 'norcalli/nvim-colorizer.lua', event = { "BufReadPost", "BufNewFile" } },
+    { "jalvesaq/Nvim-R",       ft = { "r" } },
+    { 'windwp/nvim-autopairs', event = 'InsertEnter' },
+    { "tpope/vim-repeat",      event = "InsertEnter" },
+    { 'tpope/vim-surround',    event = { "BufReadPost", "BufNewFile" } },
+    { 'tpope/vim-commentary',  event = { "BufReadPost", "BufNewFile" } },
+    {
+        'norcalli/nvim-colorizer.lua',
+        event = { "BufReadPost", "BufNewFile" },
+        keys = {
+            { "<leader>ca", "<cmd>ColorizerAttachToBuffer<cr>", desc = "Attach colorizer to the currect buffer" }
+        }
+    },
 }
