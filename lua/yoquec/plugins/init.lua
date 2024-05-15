f46505eacaaacf03ae5eccc257c5480a7ade21b0
Yoquec <alvaro.viejo@pm.me>
2024-05-15 13:14:50 +0200

    style: removes todo comments config

--- lua/yoquec/plugins/init.lua
+++ lua/yoquec/plugins/init.lua
@@ -1,10 +1,11 @@
 return {
     -- Plugins that don't need a configuration
-    { "jalvesaq/Nvim-R",       ft = { "r" } },
-    { 'windwp/nvim-autopairs', event = 'InsertEnter' },
-    { "tpope/vim-repeat",      event = "InsertEnter" },
-    { 'tpope/vim-surround',    event = { "BufReadPost", "BufNewFile" } },
-    { 'tpope/vim-commentary',  event = { "BufReadPost", "BufNewFile" } },
+    { "jalvesaq/Nvim-R",          ft = { "r" } },
+    { 'windwp/nvim-autopairs',    event = 'InsertEnter' },
+    { "tpope/vim-repeat",         event = "InsertEnter" },
+    { 'tpope/vim-surround',       event = { "BufReadPost", "BufNewFile" } },
+    { 'tpope/vim-commentary',     event = { "BufReadPost", "BufNewFile" } },
+    { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, event = { "BufReadPre", "BufNewFile" } },
     {
         'norcalli/nvim-colorizer.lua',
         event = { "BufReadPost", "BufNewFile" },
--- lua/yoquec/plugins/todo-commments.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'folke/todo-comments.nvim',
-    opts = {},
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    event = { "BufReadPre", "BufNewFile" },
-    keys = {
-        { "<Leader>ft", vim.cmd.TodoTelescope }
-    },
-}
