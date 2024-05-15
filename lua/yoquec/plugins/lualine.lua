09cfd43d8cc4c702f7ec0c309376b30b38291f1d
Yoquec <alvaro.viejo@pm.me>
2024-05-15 13:29:58 +0200

    fix: fixes lualine config

--- lua/yoquec/plugins/lualine.lua
+++ lua/yoquec/plugins/lualine.lua
@@ -76,122 +76,125 @@ end
 
 return {
     'nvim-lualine/lualine.nvim',
-    keys = { { "n", "<leader>vlt", toggle_bar, "Toggle lualine" } },
-    setup = {
-        options = {
-            component_separators = '',
-            section_separators = '',
-            theme = {
-                -- We are going to use lualine_c an lualine_x as left and
-                -- right section. Both are highlighted by c theme .  So we
-                -- are just setting default looks o statusline
-                normal = { c = { fg = fg, bg = bg } },
-                inactive = { c = { fg = fg, bg = bg } },
-            },
-        },
-        sections = {
-            -- these are to remove the defaults
-            lualine_a = {},
-            lualine_b = {},
-            lualine_y = {},
-            lualine_z = {},
-            -- Left of the line
-            lualine_c = {
-                {
-                    'mode',
-                    color = get_state_color,
-                    padding = { right = 1, left = 1 },
+    config = function()
+        local lualine = require('lualine')
+        vim.keymap.set("n", "<leader>vlt", toggle_bar, { desc = "Toggle lualine" });
+        lualine.setup({
+            options = {
+                component_separators = '',
+                section_separators = '',
+                theme = {
+                    -- We are going to use lualine_c an lualine_x as left and
+                    -- right section. Both are highlighted by c theme .  So we
+                    -- are just setting default looks o statusline
+                    normal = { c = { fg = fg, bg = bg } },
+                    inactive = { c = { fg = fg, bg = bg } },
                 },
-                {
-                    get_icon,
-                    color = function()
-                        local _, color = require("nvim-web-devicons").get_icon_color_by_filetype(vim.api.nvim_eval(
-                            "&filetype"))
-                        if color == nil then
-                            color = fg
-                        end
-                        return { fg = color }
-                    end,
-                    padding = { right = 0, left = 2 },
-                    cond = conditions.buffer_not_empty,
-                },
-                {
-                    'filename',
-                    cond = conditions.buffer_not_empty,
-                    color = { fg = fg, gui = 'bold' },
-                }
             },
-            -- Right of the line
-            lualine_x = {
-                {
-                    'diagnostics',
-                    sources = { 'nvim_diagnostic' },
-                    symbols = { error = '● ', warn = '● ', info = '● ', hint = '● ' },
-                    diagnostics_color = {
-                        error = { fg = components.diagnostics.error },
-                        warn = { fg = components.diagnostics.warn },
-                        info = { fg = components.diagnostics.info },
-                        hint = { fg = components.diagnostics.hint },
+            sections = {
+                -- these are to remove the defaults
+                lualine_a = {},
+                lualine_b = {},
+                lualine_y = {},
+                lualine_z = {},
+                -- Left of the line
+                lualine_c = {
+                    {
+                        'mode',
+                        color = get_state_color,
+                        padding = { right = 1, left = 1 },
+                    },
+                    {
+                        get_icon,
+                        color = function()
+                            local _, color = require("nvim-web-devicons").get_icon_color_by_filetype(vim.api.nvim_eval(
+                                "&filetype"))
+                            if color == nil then
+                                color = fg
+                            end
+                            return { fg = color }
+                        end,
+                        padding = { right = 0, left = 2 },
+                        cond = conditions.buffer_not_empty,
+                    },
+                    {
+                        'filename',
+                        cond = conditions.buffer_not_empty,
+                        color = { fg = fg, gui = 'bold' },
+                    }
+                },
+                -- Right of the line
+                lualine_x = {
+                    {
+                        'diagnostics',
+                        sources = { 'nvim_diagnostic' },
+                        symbols = { error = '● ', warn = '● ', info = '● ', hint = '● ' },
+                        diagnostics_color = {
+                            error = { fg = components.diagnostics.error },
+                            warn = { fg = components.diagnostics.warn },
+                            info = { fg = components.diagnostics.info },
+                            hint = { fg = components.diagnostics.hint },
+                        },
+                    },
+                    {
+                        'branch',
+                        icon = '',
+                        color = { fg = fg, gui = 'bold' },
+                    },
+                    {
+                        'diff',
+                        symbols = { added = '+ ', modified = '~ ', removed = '- ' },
+                        diff_color = {
+                            added = { fg = components.vcs.added },
+                            modified = { fg = components.vcs.modified },
+                            removed = { fg = components.vcs.removed },
+                        },
+                        cond = conditions.hide_in_width,
+                    },
+                    {
+                        'location',
+                        padding = { right = 1, left = 1 },
+                    },
+                    {
+                        'progress',
+                        color = { fg = fg, gui = 'bold' },
+                        padding = { right = 2, left = 1 },
+                    },
+                    {
+                        'o:encoding', -- option component same as &encoding in viml
+                        cond = conditions.hide_in_width,
+                        color = get_state_color,
+                    },
+                    {
+                        'fileformat',
+                        icons_enabled = true,
+                        color = get_state_color,
                     },
                 },
-                {
-                    'branch',
-                    icon = '',
-                    color = { fg = fg, gui = 'bold' },
-                },
-                {
-                    'diff',
-                    symbols = { added = '+ ', modified = '~ ', removed = '- ' },
-                    diff_color = {
-                        added = { fg = components.vcs.added },
-                        modified = { fg = components.vcs.modified },
-                        removed = { fg = components.vcs.removed },
+            },
+            inactive_sections = {
+                -- these are to remove the defaults
+                lualine_a = {},
+                lualine_b = {},
+                lualine_y = {},
+                lualine_z = {},
+                lualine_x = {},
+                -- Insert left
+                lualine_c = {
+                    {
+                        get_icon,
+                        color = { fg = components.ui.bg.dark, bg = components.ui.accent },
+                        padding = { right = 1, left = 1 },
+                        cond = conditions.buffer_not_empty,
                     },
-                    cond = conditions.hide_in_width,
-                },
-                {
-                    'location',
-                    padding = { right = 1, left = 1 },
-                },
-                {
-                    'progress',
-                    color = { fg = fg, gui = 'bold' },
-                    padding = { right = 2, left = 1 },
-                },
-                {
-                    'o:encoding',     -- option component same as &encoding in viml
-                    cond = conditions.hide_in_width,
-                    color = get_state_color,
-                },
-                {
-                    'fileformat',
-                    icons_enabled = true,
-                    color = get_state_color,
+                    {
+                        'filename',
+                        cond = conditions.buffer_not_empty,
+                        color = { fg = components.ui.bg.dark, bg = components.ui.accent, gui = 'bold' },
+                        padding = { right = 1, left = 0 },
+                    },
                 },
             },
-        },
-        inactive_sections = {
-            -- these are to remove the defaults
-            lualine_a = {},
-            lualine_b = {},
-            lualine_y = {},
-            lualine_z = {},
-            lualine_x = {},
-            -- Insert left
-            lualine_c = {
-                {
-                    get_icon,
-                    color = { fg = components.ui.bg.dark, bg = components.ui.accent },
-                    padding = { right = 1, left = 1 },
-                    cond = conditions.buffer_not_empty,
-                },
-                {
-                    'filename',
-                    cond = conditions.buffer_not_empty,
-                    color = { fg = components.ui.bg.dark, bg = components.ui.accent, gui = 'bold' },
-                    padding = { right = 1, left = 0 },
-                },
-            },
-        },
-    }
+        })
+    end
 }
