5d7ceb08b48b3b4c1df1d93db15a6a1898fdf979
Yoquec <alvaro.viejo@pm.me>
2024-07-06 23:28:39 +0200

    fix: correctly initialices nvim autopairs

--- lua/yoquec/plugins/init.lua
+++ lua/yoquec/plugins/init.lua
@@ -1,11 +1,16 @@
 return {
     -- Plugins that don't need a configuration
-    { "jalvesaq/Nvim-R",          ft = { "r" } },
-    { 'windwp/nvim-autopairs',    event = 'InsertEnter' },
-    { "tpope/vim-repeat",         event = "InsertEnter" },
-    { 'tpope/vim-surround',       event = { "BufReadPost", "BufNewFile" } },
-    { 'tpope/vim-commentary',     event = { "BufReadPost", "BufNewFile" } },
-    { 'folke/todo-comments.nvim', event = { "BufReadPost", "BufNewFile" }, opts = {}, dependencies = { 'nvim-lua/plenary.nvim' }, },
+    { "jalvesaq/Nvim-R",       ft = { "r" } },
+    { "tpope/vim-repeat",      event = "InsertEnter" },
+    { 'tpope/vim-surround',    event = { "BufReadPost", "BufNewFile" } },
+    { 'tpope/vim-commentary',  event = { "BufReadPost", "BufNewFile" } },
+    { 'windwp/nvim-autopairs', event = { 'InsertEnter' },              opts = {} },
+    {
+        'folke/todo-comments.nvim',
+        opts = {},
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
     {
         'norcalli/nvim-colorizer.lua',
         event = { "BufReadPost", "BufNewFile" },
