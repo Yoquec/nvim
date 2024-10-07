cdb4e40b0928ee1d53b4c2905246d455900723ef
Yoquec <alvaro.viejo@yoquec.com>
2024-10-07 22:25:28 +0200

    refactor: simplified dashboard

--- lua/yoquec/plugins/theme.lua
+++ lua/yoquec/plugins/theme.lua
@@ -1,6 +1,11 @@
 local accent = "#fbf1c7"
 local accent_dim = "#808080"
 local black = "#000000"
+local green = "#98971A"
+local purple = "#B16286"
+local yellow = "#EEBD35"
+local red = "#CC241C"
+local cyan = "#8EC07E"
 
 return {
     "ellisonleao/gruvbox.nvim",
@@ -17,7 +22,12 @@ return {
                 StatusLine = { fg = accent, bg = "none" },
                 StatusLineNC = { fg = accent_dim, bg = "none" },
                 LspInfoBorder = { fg = accent },
-                TelescopeTitle = { fg = black, bg = accent }
+                TelescopeTitle = { fg = black, bg = accent },
+                DashboardHeader = { fg = green },
+                DashboardDesc = { fg = purple },
+                DashboardShortCut = { fg = yellow },
+                DashboardKey = { fg = red },
+                DashboardFooter = { fg = cyan }
             }
         })
 
