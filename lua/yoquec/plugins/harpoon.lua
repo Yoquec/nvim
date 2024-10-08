2e62f103dc2a749db058a45ec8ff9af9d6db2e38
Yoquec <alvaro.viejo@yoquec.com>
2024-10-09 00:46:04 +0200

    refactor(harpoon): adds descriptions to keymaps

--- lua/yoquec/plugins/harpoon.lua
+++ lua/yoquec/plugins/harpoon.lua
@@ -2,19 +2,20 @@ return {
     'ThePrimeagen/harpoon',
     lazy = false,
     branch = "harpoon2",
-    config = function ()
+    config = function()
         local harpoon = require("harpoon")
         harpoon:setup()
 
         vim.keymap.set("n", "<leader>a", function()
             harpoon:list():append()
             print("👉 File added to harpoon")
-        end)
+        end, { desc = "Add current file to Harpoon menu" })
 
         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>fm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>fm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            { desc = "Harpoon menu" })
     end
 }
