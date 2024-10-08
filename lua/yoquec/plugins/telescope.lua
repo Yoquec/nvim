f4f9a64f578f8b9c772d39f40fd4484dbf03a343
Yoquec <alvaro.viejo@yoquec.com>
2024-10-09 00:37:14 +0200

    refactor(telescope): cleans up keymaps

--- lua/yoquec/plugins/telescope.lua
+++ lua/yoquec/plugins/telescope.lua
@@ -13,7 +13,6 @@ return {
 
         require("telescope").setup {
             defaults = {
-                -- set the prompt at the top
                 layout_config = {
                     horizontal = {
                         prompt_position = "top"
@@ -28,9 +27,9 @@ return {
                 diagnostics = { theme = "dropdown" },
                 git_status = { theme = "dropdown" },
                 git_branches = { theme = "dropdown" },
-                lsp_type_definitions = {theme = "dropdown"},
-                lsp_document_symbols = {theme = "dropdown"},
-                lsp_workspace_symbols = {theme = "dropdown"},
+                lsp_type_definitions = { theme = "dropdown" },
+                lsp_document_symbols = { theme = "dropdown" },
+                lsp_workspace_symbols = { theme = "dropdown" },
             },
             extensions = {
                 file_browser = {
@@ -54,29 +53,66 @@ return {
         require("telescope").load_extension("file_browser")
         require("telescope").load_extension("ui-select")
 
-        vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fp', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fc', builtin.commands, {})
-        vim.keymap.set('n', '<leader>fM', builtin.marks, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
-        vim.keymap.set('n', '<Leader>fi', builtin.builtin, {})
-        vim.keymap.set('n', '<Leader>fd', builtin.diagnostics, {})
-        vim.keymap.set('n', '<Leader>fs', builtin.spell_suggest, {})
-        vim.keymap.set('n', '<Leader>fq', builtin.current_buffer_fuzzy_find, {})
-        vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fgc', builtin.git_commits, {})
-        vim.keymap.set('n', '<leader>fgb', builtin.git_branches, {})
-        vim.keymap.set('n', '<leader>fgs', builtin.git_status, {})
-        vim.keymap.set('n', '<Leader>flr', builtin.lsp_references, {})
-        vim.keymap.set('n', '<Leader>fld', builtin.lsp_definitions, {})
-        vim.keymap.set('n', '<Leader>fli', builtin.lsp_implementations, {})
-        vim.keymap.set('n', '<Leader>flt', builtin.lsp_type_definitions, {})
-        vim.keymap.set('n', '<Leader>fls', builtin.lsp_document_symbols, {})
-        vim.keymap.set('n', '<Leader>flw', builtin.lsp_workspace_symbols, {})
-        vim.keymap.set('n', '<leader>fv', [[<cmd>Telescope file_browser<cr>]], {})
-        vim.keymap.set('n', "<Leader>ft", [[<cmd>TodoTelescope theme=dropdown<cr>]], {})
+        -- Extensions
+        vim.keymap.set('n', '<leader>fv', [[<cmd>Telescope file_browser<cr>]],
+            { desc = "Telescope file browser" })
+        vim.keymap.set('n', "<Leader>ft", [[<cmd>TodoTelescope theme=dropdown<cr>]],
+            { desc = "Telescope ToDo picker" })
+
+        -- File search
+        vim.keymap.set('n', '<Leader>ff', builtin.find_files,
+            { desc = "Telescope find files" })
+        vim.keymap.set('n', '<leader>fo', builtin.oldfiles,
+            { desc = "Telescope find old files" })
+        vim.keymap.set('n', '<leader>fgf', builtin.git_files,
+            { desc = "Telescope git files picker" })
+
+        -- Content searching
+        vim.keymap.set('n', '<leader>fp', builtin.live_grep,
+            { desc = "Telescope live grep" })
+        vim.keymap.set('n', '<Leader>fq', builtin.current_buffer_fuzzy_find,
+            { desc = "Telescope current buffer fuzzy find" })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers,
+            { desc = "Telescope open buffer picker" })
+        vim.keymap.set('n', '<leader>fM', builtin.marks,
+            { desc = "Telescope marks picker" })
+
+        -- Git operations
+        vim.keymap.set('n', '<leader>fgc', builtin.git_commits,
+            { desc = "Telescope git commit picker" })
+        vim.keymap.set('n', '<leader>fgb', builtin.git_branches,
+            { desc = "Telescope git branches picker" })
+        vim.keymap.set('n', '<leader>fgs', builtin.git_status,
+            { desc = "Telescope git status" })
+        vim.keymap.set('n', '<leader>fgS', builtin.git_stash,
+            { desc = "Telescope git stash picker" })
+
+        -- LSP operations
+        vim.keymap.set('n', '<Leader>flr', builtin.lsp_references,
+            { desc = "Telescope LSP references" })
+        vim.keymap.set('n', '<Leader>fld', builtin.lsp_definitions,
+            { desc = "Telescope LSP definitions" })
+        vim.keymap.set('n', '<Leader>fli', builtin.lsp_implementations,
+            { desc = "Telescope LSP implementations" })
+        vim.keymap.set('n', '<Leader>flt', builtin.lsp_type_definitions,
+            { desc = "Telescope LSP type definitions" })
+        vim.keymap.set('n', '<Leader>fls', builtin.lsp_document_symbols,
+            { desc = "Telescope LSP document symbols" })
+        vim.keymap.set('n', '<Leader>flw', builtin.lsp_workspace_symbols,
+            { desc = "Telescope LSP workspace symbols" })
+        vim.keymap.set('n', '<Leader>fd', builtin.diagnostics,
+            { desc = "Telescope LSP diagnostics" })
+
+        -- Misc
+        vim.keymap.set('n', '<leader>fc', builtin.commands,
+            { desc = "Telescope command picker" })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+            { desc = "Telescope help picker" })
+        vim.keymap.set('n', '<leader>fk', builtin.keymaps,
+            { desc = "Telescope keymap picker" })
+        vim.keymap.set('n', '<Leader>fi', builtin.builtin,
+            { desc = "Telescope picker picker (lol)" })
+        vim.keymap.set('n', '<Leader>fs', builtin.spell_suggest,
+            { desc = "Telescope spelling suggestions" })
     end
 }
