8611a4cf63fd51fbc88d2af29de96b7aa6b5ec0f
Yoquec <alvaro.viejo@yoquec.com>
2024-10-12 17:40:44 +0200

    fix: updates theme colors

--- lua/yoquec/plugins/theme.lua
+++ lua/yoquec/plugins/theme.lua
@@ -1,33 +1,33 @@
-local accent = "#fbf1c7"
-local accent_dim = "#808080"
-local black = "#000000"
+local accent = "#ebdbb2"
+local accent_dim = "#32302f"
 local green = "#98971A"
 local purple = "#B16286"
 local yellow = "#EEBD35"
 local red = "#FB4934"
 local cyan = "#8EC07E"
-local orange = "#FB4934"
+local orange = "#FE8019"
 local gray = "#A89984"
+local bg1 = "#3C3836"
 
 return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
         require("gruvbox").setup({
-            contrast = "soft",
+            contrast = "hard",
             transparent_mode = true,
             invert_selection = true,
             overrides = {
                 LineNr = { fg = accent },
-                LineNrAbove = { fg = accent_dim },
-                LineNrBelow = { fg = accent_dim },
+                LineNrAbove = { fg = bg1 },
+                LineNrBelow = { fg = bg1 },
 
                 StatusLine = { fg = accent, bg = "none" },
                 StatusLineNC = { fg = accent_dim, bg = "none" },
 
                 LspInfoBorder = { fg = accent },
 
-                TelescopeTitle = { fg = black, bg = accent },
+                TelescopeTitle = { fg = accent_dim, bg = accent },
 
                 DashboardHeader = { fg = green },
                 DashboardDesc = { fg = purple },
