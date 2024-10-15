d4257fb3ee9df1a12d5a60e143c7a742acc0c91c
Yoquec <alvaro.viejo@yoquec.com>
2024-10-15 21:20:02 +0200

    feat(writting): adds configuration for latex

--- lua/yoquec/core/writting/init.lua
+++ lua/yoquec/core/writting/init.lua
@@ -2,3 +2,4 @@ vim.api.nvim_create_augroup("writting", {})
 
 require("yoquec.core.writting.markdown")
 require("yoquec.core.writting.rmarkdown")
+require("yoquec.core.writting.latex")
---
+++ lua/yoquec/core/writting/latex.lua
@@ -0,0 +1,55 @@
+local viewers = require("yoquec.core.writting.viewers")
+local callbacks = require("yoquec.core.writting.callbacks")
+
+local function render_pdf()
+    local file_path = vim.fn.expand("%:p")
+    vim.fn.jobstart(
+        [[bash -c 'latexmk -pdf "]] .. file_path .. [["]],
+        { on_exit = callbacks.document_render_finish }
+    )
+    print("Creating latex pdf document with latexmk...")
+end
+
+local function render_pdf_sync()
+    local file_path = vim.fn.expand("%:p")
+    vim.cmd([[!latexmk -pdf "]] .. file_path .. [["]])
+end
+
+local function render_makefile()
+    vim.fn.jobstart(
+        [[bash -c 'make"]],
+        { on_exit = callbacks.document_render_finish }
+    )
+    print("Creating latex document from 'make' command...")
+end
+
+local function render_makefile_sync()
+    vim.cmd([[make]])
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = "writting",
+    pattern = "tex",
+    callback = function(args)
+        vim.keymap.set('n', '<leader>rlp', render_pdf, {
+            buffer = args.buf,
+            desc = "Render LaTeX pdf document with latexmk"
+        })
+        vim.keymap.set('n', '<leader><leader>rlp', render_pdf_sync, {
+            buffer = args.buf,
+            desc = "Render LaTeX pdf document with latexmk (foreground)"
+        })
+        vim.keymap.set('n', '<leader>rlm', render_makefile, {
+            buffer = args.buf,
+            desc = "Render LaTeX using the 'make' command"
+        })
+        vim.keymap.set('n', '<leader><leader>rlm', render_makefile_sync, {
+            buffer = args.buf,
+            desc = "Render LaTeX using the 'make' command (foreground)"
+        })
+        vim.keymap.set('n', '<leader>vp', viewers.open_pdf, {
+            buffer = args.buf,
+            desc = "Open the compiled latex pdf file in a pdf viewer"
+        })
+    end
+})
