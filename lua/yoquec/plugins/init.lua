693e560707434e0c0ad477ea308968deed959365
Alvaro Viejo <alvaro.viejo@yoquec.com>
2024-11-10 23:46:46 +0100

    fix: removes vim comentary as it is included in neovim 0.10

--- lua/yoquec/plugins/init.lua
+++ lua/yoquec/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
     { "tpope/vim-repeat",     event = "InsertEnter" },
     { 'tpope/vim-surround',   event = { "BufReadPost", "BufNewFile" } },
-    { 'tpope/vim-commentary', event = { "BufReadPost", "BufNewFile" } },
     {
         'windwp/nvim-autopairs',
         opts = {},
