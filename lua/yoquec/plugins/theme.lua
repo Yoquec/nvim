5f1a5e8938d2deff5d94f0d8573e2522642bd6f3
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-01-18 14:34:06 +0100

    fix(theme): remove hardcoded colors and use palette

--- lua/yoquec/plugins/theme.lua
+++ lua/yoquec/plugins/theme.lua
@@ -1,18 +1,18 @@
-local accent = "#EBDBB2"
-local accent_dim = "#32302F"
-local green = "#B8BB26"
-local purple = "#B16286"
-local yellow = "#EEBD35"
-local red = "#FB4934"
-local cyan = "#8EC07E"
-local orange = "#FE8019"
-local gray = "#A89984"
-local bg3 = "#665C54"
-
 return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
+        local palette = require("gruvbox").palette
+
+        local accent, accent_alt
+        if vim.o.bg == "dark" then
+            accent =  palette.light0_soft
+            accent_alt = palette.dark0_soft
+        else
+            accent =  palette.dark0_soft
+            accent_alt = palette.light0_soft
+        end
+
         require("gruvbox").setup({
             contrast = "hard",
             transparent_mode = true,
@@ -26,42 +26,42 @@ return {
               },
             overrides = {
                 LineNr = { fg = accent },
-                LineNrAbove = { fg = bg3 },
-                LineNrBelow = { fg = bg3 },
-                TreesitterContextLineNumber = { fg = accent_dim, bg = accent },
-                TreesitterContextLineNumberBottom = { fg = accent_dim, bg = accent },
+                LineNrAbove = { fg = palette.dark4 },
+                LineNrBelow = { fg = palette.dark4 },
+                TreesitterContextLineNumber = { fg = accent_alt, bg = accent },
+                TreesitterContextLineNumberBottom = { fg = accent_alt, bg = accent },
 
-                StatusLine = { fg = accent, bg = accent_dim },
-                StatusLineNC = { fg = accent_dim, bg = accent },
+                StatusLine = { fg = accent, bg = accent_alt },
+                StatusLineNC = { fg = accent_alt, bg = accent },
 
                 LspInfoBorder = { fg = accent },
 
-                TelescopeTitle = { fg = accent_dim, bg = accent },
+                TelescopeTitle = { fg = accent_alt, bg = accent },
 
-                DashboardHeader = { fg = green },
-                DashboardDesc = { fg = purple },
-                DashboardShortCut = { fg = yellow },
-                DashboardKey = { fg = red },
-                DashboardFooter = { fg = cyan },
+                DashboardHeader = { fg = palette.neutral_green },
+                DashboardDesc = { fg = palette.neutral_purple },
+                DashboardShortCut = { fg = palette.neutral_yellow },
+                DashboardKey = { fg = palette.neutral_red },
+                DashboardFooter = { fg = palette.neutral_aqua },
 
                 -- Markdown
-                ["@markup.heading.1.marker.markdown"] = { fg = red, bold = true },
-                ["@markup.heading.1.markdown"] = { fg = red, bold = true },
-                ["@markup.heading.2.marker.markdown"] = { fg = orange, bold = true },
-                ["@markup.heading.2.markdown"] = { fg = orange, bold = true },
-                ["@markup.heading.3.marker.markdown"] = { fg = yellow, bold = true },
-                ["@markup.heading.3.markdown"] = { fg = yellow, bold = true },
-                ["@markup.heading.4.marker.markdown"] = { fg = green, bold = true },
-                ["@markup.heading.4.markdown"] = { fg = green, bold = true },
-                ["@markup.heading.5.marker.markdown"] = { fg = cyan, bold = true },
-                ["@markup.heading.5.markdown"] = { fg = cyan, bold = true },
-                ["@markup.heading.6.marker.markdown"] = { fg = purple, bold = true },
-                ["@markup.heading.6.markdown"] = { fg = purple, bold = true },
+                ["@markup.heading.1.marker.markdown"] = { fg = palette.neutral_red, bold = true },
+                ["@markup.heading.1.markdown"] = { fg = palette.neutral_red, bold = true },
+                ["@markup.heading.2.marker.markdown"] = { fg = palette.neutral_aqua, bold = true },
+                ["@markup.heading.2.markdown"] = { fg = palette.neutral_aqua, bold = true },
+                ["@markup.heading.3.marker.markdown"] = { fg = palette.neutral_yellow, bold = true },
+                ["@markup.heading.3.markdown"] = { fg = palette.neutral_yellow, bold = true },
+                ["@markup.heading.4.marker.markdown"] = { fg = palette.neutral_green, bold = true },
+                ["@markup.heading.4.markdown"] = { fg = palette.neutral_green, bold = true },
+                ["@markup.heading.5.marker.markdown"] = { fg = palette.neutral_aqua, bold = true },
+                ["@markup.heading.5.markdown"] = { fg = palette.neutral_aqua, bold = true },
+                ["@markup.heading.6.marker.markdown"] = { fg = palette.neutral_purple, bold = true },
+                ["@markup.heading.6.markdown"] = { fg = palette.neutral_purple, bold = true },
 
-                ["@markup.strong.markdown_inline"] = { fg = cyan, bold = true },
-                ["@markup.italic.markdown_inline"] = { fg = green, italic = true },
-                ["@markup.raw.markdown_inline"] = { fg = red, bg = accent_dim },
-                ["@markup.quote.markdown"] = { fg = gray, italic = true },
+                ["@markup.strong.markdown_inline"] = { fg = palette.neutral_aqua, bold = true },
+                ["@markup.italic.markdown_inline"] = { fg = palette.neutral_green, italic = true },
+                ["@markup.raw.markdown_inline"] = { fg = palette.neutral_red, bg = accent_alt },
+                ["@markup.quote.markdown"] = { fg = palette.gray, italic = true },
             }
         })
 
