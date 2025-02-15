8f38a402b73f163fc399cc55a258dfe8854870ba
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-02-16 00:40:30 +0100

    build: adds lua development flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Basic flake for neovim config lua development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in 
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            lua-language-server
+            stylua
+          ];
+        };
+  });
+  };
+}
