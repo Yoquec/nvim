2ca44764852c83c2535771a5d430e9a8c4491b1a
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-05-18 15:38:13 +0200

    refactor(snacks): uses Snacks global variable

--- lua/yoquec/plugins/snacks.lua
+++ lua/yoquec/plugins/snacks.lua
@@ -3,7 +3,7 @@ local conceal = vim.opt.conceallevel
 local function zen_on_open(_)
 	vim.opt.wrap = true
 	vim.opt.nu = false
-    vim.opt.rnu = false
+	vim.opt.rnu = false
 	vim.opt.conceallevel = 0
 end
 
@@ -16,7 +16,7 @@ end
 
 return {
 	"folke/snacks.nvim",
-    lazy = false,
+	lazy = false,
 	opts = {
 		notifier = {},
 		input = {},
@@ -33,12 +33,8 @@ return {
 		},
 	},
 	keys = {
-		{
-			"<leader>zz",
-			function()
-				require("snacks").zen.zen()
-			end,
-			desc = "Toggle [Z]en mode",
-		},
+        -- stylua: ignore start
+        { "<leader>zz", function() Snacks.zen.zen() end, desc = "Toggle [Z]en mode" },
+		-- stylua: ignore end
 	},
 }
