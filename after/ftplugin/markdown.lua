6ee2faa714d081465beddb9526e8e89741ac5608
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-06-07 11:42:52 +0200

    chore: enables wrap in tex and markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,6 +5,7 @@ vim.opt_local.conceallevel = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
+vim.opt_local.wrap = true
 
 --- Creates pdf document with pandoc (in the foreground)
 local function render_pdf()
