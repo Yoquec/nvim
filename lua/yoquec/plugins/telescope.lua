a34a57f4635058c19872a6c5db1600e31c9f9fb0
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-06-08 21:09:27 +0200

    chore: replace telescope file explorer with snacks file explorer

--- lua/yoquec/plugins/telescope.lua
+++ lua/yoquec/plugins/telescope.lua
@@ -8,16 +8,12 @@ return {
 	},
 	keys = {
         -- stylua: ignore start
-		{ "<leader>fv", [[<cmd>Telescope file_browser theme=ivy<cr>]], desc = "Telescope file browser" },
-
 		{ "<Leader>fli", [[<cmd>Telescope lsp_incoming_calls theme=dropdown<cr>]], desc = "Telescope LSP incoming calls" },
 		{ "<Leader>flo", [[<cmd>Telescope lsp_outgoing_calls theme=dropdown<cr>]], desc = "Telescope LSP outgoing calls" },
 		-- stylua: ignore end
 	},
 	config = function()
 		local telescope = require("telescope")
-		local actions = require("telescope.actions")
-		local fb_actions = require("telescope").extensions.file_browser.actions
 
 		telescope.setup({
 			defaults = {
@@ -27,22 +23,7 @@ return {
 					},
 				},
 			},
-			pickers = {},
-			extensions = {
-				file_browser = {
-					path = "%:p:h",
-					mappings = {
-						["n"] = {
-							["%"] = fb_actions.create,
-							["<C-n>"] = actions.move_selection_next,
-							["<C-p>"] = actions.move_selection_previous,
-						},
-					},
-				},
-			},
 		})
-
-		telescope.load_extension("file_browser")
 
 		-- HACK: Fix telescope borders until the fix for plenary 0.11 gets merged
 		-- See: https://github.com/nvim-lua/plenary.nvim/pull/649
