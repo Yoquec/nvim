0a126717e640cdd3c982e52660e1c6fd53e62fcd
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-08-18 21:53:44 +0200

    chore: cleans up dap config

--- lua/yoquec/plugins/dap.lua
+++ lua/yoquec/plugins/dap.lua
@@ -1,75 +1,18 @@
 return {
 	"mfussenegger/nvim-dap",
-	ft = { "go", "javascript", "javascriptreact", "typescript", "typescriptreact" },
 	dependencies = { "leoluz/nvim-dap-go" },
 	config = function()
-		local dap = require("dap")
-		local dap_ui = require("dap.ui.widgets")
-
-		-- Keymaps
-		vim.keymap.set("n", "<leader>Dc", dap.continue)
-		vim.keymap.set("n", "<leader>Dq", dap.close)
-		vim.keymap.set("n", "<leader>Db", dap.toggle_breakpoint)
-		vim.keymap.set("n", "<leader>Do", dap.step_over)
-		vim.keymap.set("n", "<leader>Di", dap.step_into)
-		vim.keymap.set("n", "<leader>Dr", dap.repl.open)
-		vim.keymap.set("n", "<leader>Dh", dap_ui.hover)
-		vim.keymap.set("n", "<leader>Dp", dap_ui.preview)
-
-		-- Signs
-		vim.fn.sign_define(
-			"DapBreakpoint",
-			{ text = "", texthl = "@dap.breakpoint", linehl = "@dap.breakpoint", numhl = "@dap.breakpoint" }
-		)
-		vim.fn.sign_define(
-			"DapBreakpointCondition",
-			{ text = "ﳁ", texthl = "@dap.breakpoint", linehl = "@dap.breakpoint", numhl = "@dap.breakpoint" }
-		)
-		vim.fn.sign_define(
-			"DapBreakpointRejected",
-			{ text = "", texthl = "@dap.breakpoint", linehl = "@dap.breakpoint", numhl = "@dap.breakpoint" }
-		)
-		vim.fn.sign_define(
-			"DapLogPoint",
-			{ text = "", texthl = "@dap.logpoint", linehl = "@dap.logpoint", numhl = "@dap.logpoint" }
-		)
-		vim.fn.sign_define(
-			"DapStopped",
-			{ text = "", texthl = "@dap.stopped", linehl = "@dap.stopped", numhl = "@dap.stopped" }
-		)
-
-		-- Golang debug adapter
 		require("dap-go").setup()
-
-		-- Javascript node debug adapter
-		dap.adapters["pwa-node"] = {
-			type = "server",
-			host = "localhost",
-			port = "${port}",
-			executable = {
-				command = "node",
-				-- Download from https://github.com/microsoft/vscode-js-debug/releases
-				args = { os.getenv("HOME") .. "/.local/share/js-debug/src/dapDebugServer.js", "${port}" },
-			},
-		}
-
-		for _, language in ipairs({ "javascript", "javascriptreact", "typescript", "typescriptreact" }) do
-			require("dap").configurations[language] = {
-				{
-					type = "pwa-node",
-					request = "launch",
-					name = "Launch file",
-					program = "${file}",
-					cwd = "${workspaceFolder}",
-				},
-				{
-					type = "pwa-node",
-					request = "attach",
-					name = "Attach",
-					processId = require("dap.utils").pick_process,
-					cwd = "${workspaceFolder}",
-				},
-			}
-		end
 	end,
+	keys = {
+		{ "<leader>Dn", vim.cmd.DapNew, desc = "Debug adapter: new" },
+		{ "<leader>Dc", vim.cmd.DapContinue, desc = "Debug adapter: continue" },
+		{ "<leader>Db", vim.cmd.DapToggleBreakpoint, desc = "Debug adapter: toggle breakpoint" },
+		{ "<leader>Di", vim.cmd.DapStepInto, desc = "Debug adapter: step into" },
+		{ "<leader>Do", vim.cmd.DapStepOver, desc = "Debug adapter: step over" },
+		{ "<leader>DO", vim.cmd.DapStepOut, desc = "Debug adapter: step out" },
+		{ "<leader>DC", vim.cmd.DapClearBreakpoints, desc = "Debug adapter: clear breakpoints" },
+		{ "<leader>Dr", vim.cmd.DapToggleRepl, desc = "Debug adapter: REPL" },
+		{ "<leader>Dq", vim.cmd.DapTerminate, desc = "Debug adapter: terminate" },
+	},
 }
