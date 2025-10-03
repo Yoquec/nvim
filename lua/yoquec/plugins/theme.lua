0a813eca8bb61ccfad77130d8dc2797f9909f6b7
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-10-03 22:55:26 +0200

    chore: removes tairiki in favor of base16

--- lua/yoquec/plugins/theme.lua
+++ lua/yoquec/plugins/theme.lua
@@ -1,122 +1,27 @@
-local color_overrides = {
-	light = {
-		fg = "#000000",
-		bg = "#ffffff",
-		red = "#c82829",
-		green = "#718c00",
-		yellow = "#eab700",
-		blue = "#4271ae",
-		purple = "#8959a8",
-		cyan = "#3e999f",
-	},
-	dark = {
-		fg = "#eaeaea",
-		bg = "#000000",
-		red = "#d54e53",
-		green = "#b9ca4a",
-		yellow = "#e7c547",
-		blue = "#7aa6da",
-		purple = "#c397d8",
-		cyan = "#70c0b1",
-	},
-}
+-- Pallete based on ghostty's Tomorrow Night Bright pallete
+-- See: https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/refs/heads/master/ghostty/Tomorrow%20Night%20Bright
 
 return {
-	"deparr/tairiki.nvim",
+	"RRethy/base16-nvim",
+	name = "base16-colorscheme",
 	lazy = false,
 	priority = 1000,
-	plugins = {
-		blink = true,
-		diffview = true,
-		gitsigns = true,
-		neovim = true,
-		nvim_tree = true,
-		semantic_tokens = true,
-		telescope = true,
-		treesitter = true,
-		treesitter_context = true,
-		which_key = true,
+	opts = {
+		base00 = "#000000",
+		base01 = "#000000",
+		base02 = "#1d1f21",
+		base03 = "#757d8a",
+		base04 = "#8b95a7",
+		base05 = "#eaeaea",
+		base06 = "#373b41",
+		base07 = "#ffffff",
+		base08 = "#d54e53",
+		base09 = "#d19a66",
+		base0A = "#e7c547",
+		base0B = "#b9ca4a",
+		base0C = "#70c0b1",
+		base0D = "#7aa6da",
+		base0E = "#c397d8",
+		base0F = "#a06949",
 	},
-	config = function()
-		require("tairiki").setup({
-			end_of_buffer = true,
-
-			diagnostics = {
-				darker = true,
-				background = true,
-				undercurl = true,
-			},
-
-			colors = function(color, _)
-				for k, v in pairs(color_overrides[vim.o.bg]) do
-					color[k] = v
-				end
-			end,
-
-			highlights = function(highlight, color, _)
-				local groups = {
-					["LineNr"] = { fg = color.fg, bold = true },
-					["LineNrAbove"] = { fg = color.fg_dark2, bold = false },
-					["LineNrBelow"] = { fg = color.fg_dark2, bold = false },
-					["NormalFloat"] = { fg = color.fg, bg = color.bg },
-					["FloatBorder"] = { fg = color.fg, bg = color.bg },
-					["Pmenu"] = { fg = color.fg, bg = color.bg },
-					["PmenuKind"] = { fg = color.fg, bg = color.bg },
-
-					["TreesitterContextLineNumber"] = { bg = color.fg, fg = color.bg },
-					["TreesitterContextLineNumberBottom"] = { bg = color.fg, fg = color.bg },
-
-					["SpellBad"] = { sp = color.red, underdotted = true },
-					["SpellCap"] = { sp = color.yellow, underdotted = true },
-					["SpellRare"] = { sp = color.purple, underdotted = true },
-					["SpellLocal"] = { sp = color.fg, underdotted = true },
-
-					["TelescopeTitle"] = { bg = color.fg, fg = color.bg },
-
-					-- TODO: Push fix to tairiki.nvim
-					["BlinkCmpKindFolder"] = { fg = color.yellow },
-					["BlinkCmpKindSnippet"] = { fg = color.red },
-					["BlinkCmpKindField"] = { fg = color.blue },
-					["BlinkCmpKindModule"] = { fg = color.green },
-					["BlinkCmpKindVariable"] = { fg = color.red },
-					["BlinkCmpKindKeyword"] = { link = "@lsp.type.keyword" },
-					["BlinkCmpKindText"] = { link = "@lsp.type.string" },
-					["BlinkCmpKindEvent"] = { link = "@lsp.type.event" },
-					["BlinkCmpKindConstant"] = { link = "Constant" },
-					["BlinkCmpKindFunction"] = { link = "@lsp.type.function" },
-					["BlinkCmpKindMethod"] = { link = "@lsp.type.method" },
-					["BlinkCmpKindClass"] = { link = "@lsp.type.class" },
-					["BlinkCmpKindStruct"] = { link = "@lsp.type.struct" },
-					["BlinkCmpKindEnum"] = { link = "@lsp.type.enum" },
-					["BlinkCmpKindEnumMember"] = { link = "@lsp.type.enumMember" },
-
-					["@obsidian.tag"] = { fg = color.yellow, underdotted = true },
-					["@obsidian.link"] = { fg = color.blue, underline = true },
-
-					["@markup.link.label"] = { fg = color.yellow },
-					["@markup.link.label.markdown_inline"] = { fg = color.blue },
-					["@markup.strong.markdown_inline"] = { fg = color.yellow, bold = true },
-					["@markup.italic.markdown_inline"] = { fg = color.purple, italic = true },
-					["@markup.raw.markdown_inline"] = { fg = color.red, bg = color.bg_light3 },
-					["@markup.quote.markdown"] = { fg = color.yellow, italic = true },
-					["@tag.attribute"] = { fg = color.yellow },
-					["@type.builtin"] = { fg = color.yellow },
-					["Boolean"] = { fg = color.yellow },
-					["Number"] = { fg = color.yellow },
-					["@property.yaml"] = { fg = color.red },
-				}
-
-				for group, config in pairs(groups) do
-					highlight[group] = config
-				end
-
-				for i = 1, 6, 1 do
-					highlight["@markup.heading." .. i .. ".marker.markdown"] = { fg = color.blue, bold = true }
-					highlight["@markup.heading." .. i .. ".markdown"] = { fg = color.blue, bold = true }
-				end
-			end,
-		})
-
-		vim.cmd.colorscheme("tairiki")
-	end,
 }
