7b5bbc366c7b1d7843ab1897d032d39745e6eb57
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-02-28 13:14:02 +0100

    chore(theme): adds support for the "THEME" env variable

--- lua/yoquec/plugins/theme.lua
+++ lua/yoquec/plugins/theme.lua
@@ -1,54 +1,68 @@
 -- Pallete based on ghostty's Tomorrow Night Bright pallete
 -- See: https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/refs/heads/master/ghostty/Tomorrow%20Night%20Bright
 
+local theme_var = os.getenv("THEME")
+
+local themes = { dark = {} }
+
+themes.dark.colors = {
+	base00 = "#000000",
+	base01 = "#000000",
+	base02 = "#000000",
+	base03 = "#757d8a",
+	base04 = "#8b95a7",
+	base05 = "#eaeaea",
+	base06 = "#373b41",
+	base07 = "#ffffff",
+	base08 = "#d54e53",
+	base09 = "#7aa6da",
+	base0A = "#e7c547",
+	base0B = "#b9ca4a",
+	base0C = "#70c0b1",
+	base0D = "#7aa6da",
+	base0E = "#c397d8",
+	base0F = "#e7c547",
+}
+
+themes.dark.group_overrides = {
+	-- Enable color inversion for selection
+	["Visual"] = { reverse = true },
+
+	-- TS tweaks
+	["TSStrong"] = { fg = themes.dark.colors.base0F, bold = true },
+	["Italic"] = { fg = themes.dark.colors.base0E, italic = true },
+
+	-- markdown tweaks
+	["@markup.list.markdown"] = { fg = themes.dark.colors.base08 },
+
+	-- Fix markdown strikethrough
+	["@markup.strikethrough.markdown_inline"] = { link = "markdownStrikeDelimiter" },
+
+	-- Fix markdown inline code
+	["Raw"] = { fg = themes.dark.colors.base08, bg = themes.dark.colors.base06 },
+	["@markup.raw.markdown_inline"] = { link = "Raw" },
+
+	-- Fix spelling highlights
+	["SpellBad"] = { sp = themes.dark.colors.base08, underdotted = true },
+	["SpellCap"] = { sp = themes.dark.colors.base09, underdotted = true },
+	["SpellRare"] = { sp = themes.dark.colors.base0E, underdotted = true },
+	["SpellLocal"] = { sp = themes.dark.colors.base0C, underdotted = true },
+}
+
 return {
 	"RRethy/base16-nvim",
 	name = "base16-colorscheme",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("base16-colorscheme").setup({
-			base00 = "#000000",
-			base01 = "#000000",
-			base02 = "#000000",
-			base03 = "#757d8a",
-			base04 = "#8b95a7",
-			base05 = "#eaeaea",
-			base06 = "#373b41",
-			base07 = "#ffffff",
-			base08 = "#d54e53",
-			base09 = "#7aa6da",
-			base0A = "#e7c547",
-			base0B = "#b9ca4a",
-			base0C = "#70c0b1",
-			base0D = "#7aa6da",
-			base0E = "#c397d8",
-			base0F = "#e7c547",
-		})
-
-		local colors = require("base16-colorscheme").colors
-
-		-- Enable color inversion for selection
-		vim.api.nvim_set_hl(0, "Visual", { reverse = true })
-
-		-- TS tweaks
-		vim.api.nvim_set_hl(0, "TSStrong", { fg = colors.base0F, bold = true })
-		vim.api.nvim_set_hl(0, "Italic", { fg = colors.base0E, italic = true })
-
-		-- markdown tweaks
-		vim.api.nvim_set_hl(0, "@markup.list.markdown", { fg = colors.base08 })
-
-		-- Fix markdown strikethrough
-		vim.api.nvim_set_hl(0, "@markup.strikethrough.markdown_inline", { link = "markdownStrikeDelimiter" })
-
-		-- Fix markdown inline code
-		vim.api.nvim_set_hl(0, "Raw", { fg = colors.base08, bg = colors.base06 })
-		vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { link = "Raw" })
-
-		-- Fix spelling highlights
-		vim.api.nvim_set_hl(0, "SpellBad", { sp = colors.base08, underdotted = true })
-		vim.api.nvim_set_hl(0, "SpellCap", { sp = colors.base09, underdotted = true })
-		vim.api.nvim_set_hl(0, "SpellRare", { sp = colors.base0E, underdotted = true })
-		vim.api.nvim_set_hl(0, "SpellLocal", { sp = colors.base0C, underdotted = true })
+		if theme_var == nil or theme_var == "dark" then
+            vim.o.background = "dark"
+			require("base16-colorscheme").setup(themes.dark.colors)
+		else
+			require("base16-colorscheme").setup()
+            -- Use default base16-tomorrow for light theme
+            vim.o.background = "light"
+			vim.cmd([[colorscheme base16-tomorrow]])
+		end
 	end,
 }
