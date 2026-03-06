295115e98ea04edf61bff5a0920f1a424eb487aa
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-03-06 22:36:01 +0100

    fix: fixes colors

--- lua/yoquec/plugins/theme.lua
+++ lua/yoquec/plugins/theme.lua
@@ -6,9 +6,9 @@ local theme_var = os.getenv("THEME")
 local themes = { dark = {} }
 
 themes.dark.colors = {
-	base00 = "#000000",
-	base01 = "#000000",
-	base02 = "#000000",
+	base00 = "none",
+	base01 = "none",
+	base02 = "none",
 	base03 = "#757d8a",
 	base04 = "#8b95a7",
 	base05 = "#eaeaea",
@@ -27,6 +27,11 @@ themes.dark.colors = {
 themes.dark.group_overrides = {
 	-- Enable color inversion for selection
 	["Visual"] = { reverse = true },
+
+	-- Highlight current line number
+	["LineNr"] = { fg = themes.dark.colors.base05, bold = true },
+	["LineNrAbove"] = { fg = themes.dark.colors.base04 },
+	["LineNrBelow"] = { fg = themes.dark.colors.base04 },
 
 	-- TS tweaks
 	["TSStrong"] = { fg = themes.dark.colors.base0F, bold = true },
@@ -56,12 +61,15 @@ return {
 	priority = 1000,
 	config = function()
 		if theme_var == nil or theme_var == "dark" then
-            vim.o.background = "dark"
+			vim.o.background = "dark"
 			require("base16-colorscheme").setup(themes.dark.colors)
+			for name, val in pairs(themes.dark.group_overrides) do
+				vim.api.nvim_set_hl(0, name, val)
+			end
 		else
 			require("base16-colorscheme").setup()
-            -- Use default base16-tomorrow for light theme
-            vim.o.background = "light"
+			-- Use default base16-tomorrow for light theme
+			vim.o.background = "light"
 			vim.cmd([[colorscheme base16-tomorrow]])
 		end
 	end,
